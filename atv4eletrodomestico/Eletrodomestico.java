5fb3cbfa8137f35fe99305af4ebc5714c95d5463
Evellynsnt <159397519+Evellynsnt@users.noreply.github.com>
2025-03-29 01:41:00 -0300

    Update Eletrodomestico.java

---
+++ atv4eletrodomestico/Eletrodomestico.java
@@ -0,0 +1,25 @@
+package atv4eletredomestico;
+
+public class Eletrodomestico {
+    private String marca;
+    private double potencia;
+
+    public Eletrodomestico(String marca, double potencia){
+        this.marca = marca;
+        this.potencia = potencia;
+    }
+
+    public String getMarca(){
+        return marca;
+    }
+    public void setMarca(String marca){
+        this.marca = marca;
+    }
+    public double getPotencia(){
+        return potencia;
+    }
+    public void setPotenvcia(double potencia){
+        this.potencia = potencia;
+    }
+
+}
